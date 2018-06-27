5fd913c6edfe2313e8b0cee6f4c0cb952488323d
brunoMTrazzi <bruno.trazzi@usp.br>
2018-06-26 22:24:00 -0300

    index.js agora usa o diretório de arquivos estáticos 'public'.

--- site/index.js
+++ site/index.js
@@ -4,17 +4,15 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.static('site/public'));
+
 app.get('/', (req, res)=>{
 //   let arquivo = await readFile("site/index.html", "utf8");
 //   console.log(arquivo);
    res.sendFile(__dirname + "/index.html");
 })
 
-app.get('/estilo.css', (req, res) => {
-   res.sendFile(__dirname + "/estilo.css");
-})
-
-let server = app.listen(8081, ()=>{
+const server = app.listen(8081, ()=>{
    let host = server.address().address
    let port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
